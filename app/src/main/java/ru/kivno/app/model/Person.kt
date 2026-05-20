271c4ffaa54765149f7f5a605ff5c6cb5c523bca
Karen Mirzoyan <5451463@gmail.com>
2026-05-20 16:41:53 +0300

    Add files via upload

--- app/src/main/java/ru/kivno/app/model/Person.kt
+++ app/src/main/java/ru/kivno/app/model/Person.kt
@@ -3,15 +3,17 @@ package ru.kivno.app.model
 import com.google.gson.annotations.SerializedName
 
 data class Person(
-    val id: Int,
-    val name: String,
-    val alias: String?,
-    val profession: String?,
-    val photo: String?,
+    val id: Int = 0,
+    val name: String = "",
+    val alias: String? = null,
+    val profession: String? = null,
+    val photo: String? = null,
+    val biography: String? = null,
+    val filmography: String? = null,
     @SerializedName("poop_count") val poopCount: Int = 0
 )
 
 data class PeopleResponse(
-    val ok: Boolean,
-    val people: List<Person>
+    val ok: Boolean = false,
+    val people: List<Person> = emptyList()
 )
